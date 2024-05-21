c77fc3e239c1867fd55063066c1ca84f012f412a
Giovanni Santos <san22523@uvg.edu.gt>
2024-05-21 02:20:10 -0600

    Update about window

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,34 @@
-function About () {
+import '../styles/about.css'
+import CVGiovanniSantos from '../assets/CVGiovanniSantos.pdf'
+import cvlogo from '../assets/cvlogo.png'
+
+function About() {
+
     return (
-      <div className="about-container">
-        <h1 className="about-title">Sobre Mi</h1>
-        <p className="about-description">lalallaa</p>
-        <p className="about-description">kakakakaka</p>
+      <div className="about-page">
+        <div className='PDF'>
+            <a href={CVGiovanniSantos} download="CVGiovanniSantos.pdf" title='Descargar CV'>
+            <img src={cvlogo} alt="Descargar CV" className="download-logo" />
+
+            </a>
+        </div>
+        <div className="about-container">
+          <h4 className='about-text'>
+            Mi nombre es Giovanni Santos y <br />  estoy en mi tercer año de
+            la carrera de Ingeniería en <br />
+            Ciencias de la Computación y Tecnologías de la Información <br />
+            en la Universidad del Valle de Guatemala. <br />
+            Durante estos tres años he desarrollado <br />
+            diferentes habilidades <br />
+            dentro y fuera del campo de la computación <br />
+            que aplico en mi día a día <br />
+            y que quiero seguir mejorando en el campo laboral.
+          </h4>
+        </div>
       </div>
-    )
+    );
   }
+  
+  
   export default About
   
